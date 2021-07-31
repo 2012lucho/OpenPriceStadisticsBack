f713b4d0f8fc85bd0e21c93416dd5ef8535102cb
berty <lucho.2012.tandil@gmail.com>
2021-07-31 19:31:12 -0300

    Se agrega codigo de otro proyecto para ver que reutilizar, de ese codigo se quito lo mas grueso, en otro commit se seguira afianando

---
+++ config/web.php
@@ -0,0 +1,136 @@
+<?php
+
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
+$config = [
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => '7hSF802zvlVYMSbq6E6hQlhCvJ-dQw0C',
+            'parsers' => [
+               'application/json' => 'yii\web\JsonParser',
+            ],
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
+            'enableAutoLogin' => false,
+            'loginUrl' => null,
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+          'class' => 'yii\swiftmailer\Mailer',
+          'transport' => [
+              'class' => 'Swift_SmtpTransport',
+              'host' => 'mail-v2.coodesoft.com.ar',  // ej. smtp.mandrillapp.com o smtp.gmail.com
+              'username' => 'postmaster@coodesoft.com.ar',
+              'password' => '**c00des0ft**',
+              'port' => '587', // El puerto 25 es un puerto común también
+              'encryption' => 'tls', // Es usado también a menudo, revise la configuración del servidor
+            ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'db' => $db,
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'enableStrictParsing' => true,
+            'showScriptName' => false,
+            'rules' => [
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'chat-room',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'gender',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'login',
+                'pluralize' => false,
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'change-password',
+                'pluralize' => false,
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'profile-image',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'matches',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'message',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'profile',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'role',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'user',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'register',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'confirm-email',
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'change-password-token',
+                'pluralize' => false,
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'password-reset',
+                'pluralize' => false,
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'resend-email-verification',
+                'pluralize' => false,
+              ],
+              [ 'class' => 'yii\rest\UrlRule',
+                'controller' => 'search-profiles',
+                'pluralize' => false,
+              ],
+            ],
+        ],
+    ],
+    'params' => $params,
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        // uncomment the following to add your IP if you are not connecting from localhost.
+        //'allowedIPs' => ['127.0.0.1', '::1'],
+    ];
+}
+
+return $config;
