8967427d5f150c0c69d8287fa3763cb2bc184c34
berty <lucho.2012.tandil@gmail.com>
2022-01-07 18:30:41 -0300

    Se crean las migraciones para las tablas de productos, precios, marcas, etc

--- config/db.php
+++ config/db.php
@@ -2,13 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=appcitas',
-    'username' => 'appcitas',
-    'password' => '1234',
+    'dsn' => 'mysql:host=localhost;dbname=precios',
+    'username' => 'root',
+    'password' => 'LuchO',
     'charset' => 'utf8',
-
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
 ];
